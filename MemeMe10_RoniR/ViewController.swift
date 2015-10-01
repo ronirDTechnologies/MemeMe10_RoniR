5094ca2f65fddf7e8cf9fc8b5bed715450bf33f0
RoniR <sourcecontrol@dinatechnologies.com>
2015-10-01 10:50:37 -0400

    Added New Features
    RR - 10-01-2015
    Added:
    1. App displays the image picker when the album button is pressed.
    2. App displays the camera when the camera button is disabled on
    devices without a camera.
    3. Various other fixes

--- MemeMe10_RoniR/ViewController.swift
+++ MemeMe10_RoniR/ViewController.swift
@@ -8,16 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var actionButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    @IBOutlet weak var memeView: UIView!
+    
+    @IBAction func performActionButton(sender: AnyObject) {
+        print("THE ACTION BUTTON HAS BEEN PRESSED")
+    }
+    
+    
+    // Display the saved photo for user to choose from existing pictures to MeMe.
+    @IBAction func performAlbumButtonAction(sender: AnyObject) {
+        
+        print("THE ALBUM BUTTON HAS BEEN PRESSED")
+        
+        showImagePickerController(.SavedPhotosAlbum)
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       setToInitialState()
+  
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // Present Image Picker Based on Source Type Provided
+    func showImagePickerController(sourceType: UIImagePickerControllerSourceType){
+        
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.sourceType = sourceType
+            imagePickerController.delegate = self
+            presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    // Display the camera for the user to take a picture
+    @IBAction func performCameraButtonAction(sender: AnyObject) {
+        
+        print("THE CAMERA BUTTON HAS BEEN PRESSED")
+        showImagePickerController(.Camera)
+    }
+    
+    // Sets everything back to the intial state
+    func setToInitialState()
+    {
+        self.actionButton.enabled = false
+        self.memeView.backgroundColor = UIColor.blackColor()
     }
 
 
