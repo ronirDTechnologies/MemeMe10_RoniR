294c6682c732e955dece5ad96a0c7bd6d6d479d1
RoniR <sourcecontrol@dinatechnologies.com>
2015-10-14 09:07:56 -0400

    UITextFields Good placement
    UITextFields Good placement

--- MemeMe10_RoniR/ViewController.swift
+++ MemeMe10_RoniR/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var actionButton: UIBarButtonItem!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var albumButton: UIBarButtonItem!
     @IBOutlet weak var memeView: UIView!
+    
+    @IBOutlet weak var topMemeTextField: UITextField!
+    @IBOutlet weak var bottomMemeTextField: UITextField!
+    @IBOutlet weak var userSelectedImage: UIImageView!
+   
     
     @IBAction func performActionButton(sender: AnyObject) {
         print("THE ACTION BUTTON HAS BEEN PRESSED")
@@ -30,6 +35,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -66,6 +73,76 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     {
         self.actionButton.enabled = false
         self.memeView.backgroundColor = UIColor.blackColor()
+        
+        // Set Text Properties
+        //TODO:
+        // FONT BOLD
+        // ALL CAPS
+        // WHITE COLOR
+        // BLACK OUTLINE
+        // SHRINK TO FIT
+        // IMPACT FONT
+        
+        // Set Placeholder attributes
+        
+        let memeAttr: defaultTextAttributes
+        
+        
+        let topPlaceHolderColor = NSAttributedString(string: "TOP", attributes: [
+                                                        NSBackgroundColorAttributeName:UIColor.clearColor(),
+                                                        NSForegroundColorAttributeName:UIColor.whiteColor(),
+                                                        NSFontAttributeName : UIFont(name: "Arial-BoldMT", size: 25)!,
+                                                        NSStrokeColorAttributeName: UIColor.blackColor(),
+                                                        NSStrokeWidthAttributeName: -3.0
+
+                                                                                ]
+                                                    )
+        let bottomPlaceHolderColor = NSAttributedString(string: "BOTTOM", attributes: [
+                                                        NSForegroundColorAttributeName:UIColor.whiteColor(),
+                                                        NSBackgroundColorAttributeName:UIColor.clearColor(),
+                                                        NSFontAttributeName : UIFont(name: "Arial-BoldMT", size: 25)!,
+                                                        NSStrokeColorAttributeName: UIColor.blackColor(),
+                                                        NSStrokeWidthAttributeName: -3.0
+                                                                                      ]
+                                                    )
+        
+        self.topMemeTextField.attributedPlaceholder = topPlaceHolderColor
+        self.topMemeTextField.borderStyle = .None
+        self.bottomMemeTextField.attributedPlaceholder = bottomPlaceHolderColor
+        self.bottomMemeTextField.borderStyle = .None
+        
+       // Set Text Attributes
+        
+        self.topMemeTextField.textAlignment = .Center
+        self.bottomMemeTextField.textAlignment = .Center
+  
+        self.topMemeTextField.textColor = UIColor.whiteColor()
+        
+        // Check to see if a camera exists on a device
+        if ((UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil) &&
+                (UIImagePickerController.availableCaptureModesForCameraDevice(.Front) != nil)){
+
+            cameraButton.enabled = true
+        }
+        else
+        {
+            cameraButton.enabled = false
+        }
+        
+        
+    }
+    
+    
+    // MARK: UIImagPickerControllerDelegate
+    // Implement delegate methods of UIImagePickerController delegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let userSelectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.userSelectedImage.image = userSelectedImage
+        dismissViewControllerAnimated(true, completion: nil)
     }
 
 
