2c4c33b14c773d020830b5270fc2b14670b8ed79
RoniR <sourcecontrol@dinatechnologies.com>
2016-05-16 23:13:55 -0400

    Add Meme Model
    RR: 05-16-2016
    Add Meme Model to hold user created meme info.

---
+++ MemeModel.swift
@@ -0,0 +1,17 @@
+//
+//  MemeModel.swift
+//  MemeMe10_RoniR
+//
+//  Created by Roni Rozenblat on 5/15/16.
+//  Copyright © 2016 Roni Rozenblat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct MemeModel{
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
